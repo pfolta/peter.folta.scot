6154ba6add25129effafc6f02ec1611b7acf5e8b
Peter Folta <mail@peterfolta.net>
2016-04-17 17:39:23 +0100

    Added Piwik Tracking Code

---
+++ _includes/js/piwik.js
@@ -0,0 +1,17 @@
+var _paq = _paq || [];
+
+_paq.push(["setDomains", ["*.peterfolta.net"]]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+
+(function () {
+    var u = "https://piwik.peterfolta.net/";
+    _paq.push(['setTrackerUrl', u + 'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript';
+    g.async = true;
+    g.defer = true;
+    g.src = u + 'piwik.js';
+    s.parentNode.insertBefore(g, s);
+})();
