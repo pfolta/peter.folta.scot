acde753616ed376279cd5a36859a86f080e1758f
Peter Folta <mail@peterfolta.net>
2024-03-26 23:58:55 +0000

    CSS reset and sane defaults

--- styles/global.ts
+++ styles/global.ts
@@ -4,8 +4,15 @@ import "inter-ui/inter.css";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    html {
+    * {
         font-family: "Inter", "system-ui", sans-serif;
+        line-height: calc(1em + 0.5rem);
+        -webkit-font-smoothing: antialiased;
+        margin: 0;
+    }
+
+    *, *::before, *::after {
+        box-sizing: border-box;
     }
 `;
 
